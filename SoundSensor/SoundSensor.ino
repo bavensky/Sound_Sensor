8dcad0ab1b62defc424d8efb90bb81512ab2f29c
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2014-05-05 22:45:07 +0700

    bavensky
    record when woof

--- SoundSensor/SoundSensor.ino
+++ SoundSensor/SoundSensor.ino
@@ -18,13 +18,14 @@
   #define ANALOG_IN 0
   #define DEBUG 0
   #define LIMIT 500
+  #define TIME 1000
   #define OUTPUT_FILE "Datalog.csv"
   
   File myFile;
   const int chipSelect = 4;
   RTC_DS1307 rtc;
   
-  int i=0, a=0;
+  int i=0, a=0, w=0;
   
   char line[50];
    
@@ -34,12 +35,13 @@
     Wire.begin(); 
     rtc.begin();
     pinMode(4, OUTPUT); 
-    
+     
     while (!SD.begin(chipSelect)) 
     { 
       Serial.println("initialization failed!");
-      delay(1000); 
+      delay(TIME); 
     }
+    
   }
   
   void loop()
@@ -47,20 +49,45 @@
     int sound_input = analogRead(ANALOG_IN);
     if(sound_input < LIMIT)
     {
-      writing(sound_input);
+      a = 1;
     }
+    
+    while(a==1)
+    {
+      /*-- Loop delay 10 seconds --*/
+      for(int b=0; b < TIME; b++)
+      {
+        int input = analogRead(ANALOG_IN);
+        if(input < LIMIT)
+        {      
+          return;
+        }
+        delay(10);
+      }
+      
+      a=2;
+    }
+    
+    while(a==2)
+    {
+      w++;
+      writing(w); 
+      delay(TIME);
+      a=0;
+    }
+    
   }
   
   void writing(int input)
   {
-    myFile = SD.open(OUTPUT_FILE, FILE_WRITE);
-    
+    myFile = SD.open(OUTPUT_FILE, FILE_WRITE);    
     if (myFile) 
     {
       DateTime now = rtc.now();
       sprintf(line, "%d/%d/%d,%d:%d:%d,%d", now.day(), now.month(), now.year(), now.hour(), now.minute(), now.second(), input);
       myFile.println(line);
       Serial.println(line);
+      delay(TIME);
       myFile.close();
     }
   }
